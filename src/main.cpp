6c26400d43b37d14a92160362367083e6050ce98
SimonRoG <prokhodasemen@gmail.com>
2025-03-24 17:30:29 +0100

    Refactor game controls and improve button handling

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,22 @@
 #define SDA_PIN 22
 #define SCL_PIN 23
 
-#define BTNS 15
+// #define BTNS 15
+// int BTNSRead = 0;
 
-int BTNSRead = 0;
+#define BTN1 27
+#define BTN2 14
+#define BTN3 12
+#define BTN4 13
+
 
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-byte X = 6;
-byte Y = 6;
-byte LEN = 6;
+byte X = 21;
+byte Y = 20;
+byte LEN = 20;
+
+enum Direction { GAME_OVER, RIGHT, DOWN, LEFT, UP };
 
 bool contains(std::deque<std::pair<byte, byte>> deque, std::pair<byte, byte> pair) {
   for (std::pair<byte, byte> px : deque)
@@ -36,10 +43,11 @@ public:
   byte x, y;
   byte lenghtPrev;
   byte lenght;
-  byte direction = 1; // 1 -> ++x ; 2 -> ++y ; 3 -> --x ; 4 -> --y
+  Direction direction = RIGHT; // 1 -> ++x ; 2 -> ++y ; 3 -> --x ; 4 -> --y ; 0 -> dead
   byte speed = 10;
   std::deque<std::pair<byte, byte>> body;
   std::pair<byte, byte> mouse;
+  unsigned long lastMoveTime = 0;
 
   Snake(byte x, byte y, byte lenght) {
     this->x = x;
@@ -51,8 +59,12 @@ public:
       body.push_back({ x - i, y });
     }
 
+    generateMouse(body, mouse);
+  }
+
+  void generateMouse(std::deque<std::pair<byte, byte>> body, std::pair<byte, byte>& mouse) {
     do {
-      mouse = std::make_pair(std::rand() % 128, std::rand() % 64);
+      mouse = std::make_pair(std::rand() % SCREEN_WIDTH, std::rand() % SCREEN_HEIGHT);
     } while (contains(body, mouse));
   }
 
@@ -66,19 +78,15 @@ public:
   }
 
   void moveTo(byte x, byte y) {
-    if (x == 128) this->x = 0;
-    if (x == 255) this->x = 127;
-    if (y == 64) this->y = 0;
-    if (y == 255) this->y = 63;
+    this->x = (x + SCREEN_WIDTH) % SCREEN_WIDTH;
+    this->y = (y + SCREEN_HEIGHT) % SCREEN_HEIGHT;
 
     if (oled.getPixel(x, y) == SSD1306_WHITE) {
       if (contains(body, std::make_pair(x, y)))
-        direction = 69;
+        direction = GAME_OVER;
       if (mouse == std::make_pair(x, y)) {
         lenght++;
-        do {
-          mouse = std::make_pair(std::rand() % 128, std::rand() % 64);
-        } while (contains(body, mouse));
+        generateMouse(body, mouse);
         oled.drawPixel(mouse.first, mouse.second, SSD1306_WHITE);
       }
     }
@@ -96,22 +104,24 @@ public:
   }
 
   void move() {
-    delay(1000 / speed);
-    switch (direction) {
-    case 1:
-      Snake::moveTo(++x, y);
-      break;
-    case 2:
-      Snake::moveTo(x, ++y);
-      break;
-    case 3:
-      Snake::moveTo(--x, y);
-      break;
-    case 4:
-      Snake::moveTo(x, --y);
-      break;
-    case 69:
-      break;
+    if (millis() - lastMoveTime >= 1000 / speed) {
+      lastMoveTime = millis();
+      switch (direction) {
+      case RIGHT:
+        Snake::moveTo(++x, y);
+        break;
+      case DOWN:
+        Snake::moveTo(x, ++y);
+        break;
+      case LEFT:
+        Snake::moveTo(--x, y);
+        break;
+      case UP:
+        Snake::moveTo(x, --y);
+        break;
+      case GAME_OVER:
+        break;
+      }
     }
   }
 };
@@ -122,6 +132,11 @@ void setup() {
   Serial.begin(115200);
   std::srand(std::time(0));
 
+  pinMode(BTN1, INPUT_PULLUP);
+  pinMode(BTN2, INPUT_PULLUP);
+  pinMode(BTN3, INPUT_PULLUP);
+  pinMode(BTN4, INPUT_PULLUP);
+
   Wire.begin(SDA_PIN, SCL_PIN);
 
   if (!oled.begin(SSD1306_SWITCHCAPVCC, I2C_ADDRESS)) {
@@ -131,14 +146,22 @@ void setup() {
   snake.start();
 }
 
+void readButtons() {
+  // BTNSRead = analogRead(BTNS);
+
+  // if (BTNSRead == 4095 && snake.direction != RIGHT && snake.direction != GAME_OVER) snake.direction = LEFT;
+  // if (BTNSRead < 3800 && BTNSRead > 3400 && snake.direction != DOWN && snake.direction != GAME_OVER) snake.direction = UP;
+  // if (BTNSRead < 3000 && BTNSRead > 2500 && snake.direction != UP && snake.direction != GAME_OVER) snake.direction = DOWN;
+  // if (BTNSRead < 2200 && BTNSRead > 1900 && snake.direction != LEFT && snake.direction != GAME_OVER) snake.direction = RIGHT;
+
+  if (digitalRead(BTN1) == LOW && snake.direction != RIGHT && snake.direction != GAME_OVER) snake.direction = LEFT;
+  if (digitalRead(BTN2) == LOW && snake.direction != DOWN && snake.direction != GAME_OVER) snake.direction = UP;
+  if (digitalRead(BTN3) == LOW && snake.direction != UP && snake.direction != GAME_OVER) snake.direction = DOWN;
+  if (digitalRead(BTN4) == LOW && snake.direction != LEFT && snake.direction != GAME_OVER) snake.direction = RIGHT;
+}
+
 void loop() {
   snake.move();
-  BTNSRead = analogRead(BTNS);
-
-  if (BTNSRead == 4095 && snake.direction != 1 && snake.direction != 69) snake.direction = 3;
-  if (BTNSRead < 3800 && BTNSRead > 3400 && snake.direction != 2 && snake.direction != 69) snake.direction = 4;
-  if (BTNSRead < 3000 && BTNSRead > 2500 && snake.direction != 4 && snake.direction != 69) snake.direction = 2;
-  if (BTNSRead < 2200 && BTNSRead > 1900 && snake.direction != 3 && snake.direction != 69) snake.direction = 1;
-  if (BTNSRead < 1000 && BTNSRead > 600 && snake.direction != 69) snake.lenght++;
+  readButtons();
 }
 
